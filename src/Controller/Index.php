e33e5d87090d3282160992f99d19da93b29d0468
user-penguin <kobzev96@gmail.com>
2019-04-20 01:39:27 +0700

    add index controller and index page

---
+++ src/Controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Index extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function number()
+    {
+        return $this->render('index.html.twig');
+    }
+}
